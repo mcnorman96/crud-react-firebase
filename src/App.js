e690b259bcb1c6b2f0cc23770067bf840047e86e
Norman Station <marcus-norman@hotmail.com>
2022-03-08 21:14:22 +0100

    chnages

--- src/App.js
+++ src/App.js
@@ -3,20 +3,20 @@ import { Switch, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
-import AddTutorial from "./components/add-project.component";
-import TutorialsList from "./components/projects-list.component";
+import Addproject from "./components/add-project.component";
+import projectsList from "./components/projects-list.component";
 
 class App extends Component {
   render() {
     return (
       <div>
         <nav className="navbar navbar-expand navbar-dark bg-dark">
-          <a href="/tutorials" className="navbar-brand">
+          <a href="/projects" className="navbar-brand">
             Norman
           </a>
           <div className="navbar-nav mr-auto">
             <li className="nav-item">
-              <Link to={"/tutorials"} className="nav-link">
+              <Link to={"/projects"} className="nav-link">
                   Projects
               </Link>
             </li>
@@ -31,8 +31,8 @@ class App extends Component {
         <div className="container mt-3">
           <h2>React Firestore CRUD</h2>
           <Switch>
-            <Route exact path={["/", "/tutorials"]} component={TutorialsList} />
-            <Route exact path="/add" component={AddTutorial} />
+            <Route exact path={["/", "/projects"]} component={projectsList} />
+            <Route exact path="/add" component={Addproject} />
           </Switch>
         </div>
       </div>
--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import * as firebase from "firebase";
 import "firebase/firestore";
 
 let config = {
-  apiKey: "AIzaSyB4VwDKW0vXb_omUE7JdP6JfEkSzhJ4vjE",
-  authDomain: "movieprototype.firebaseapp.com",
-  databaseURL: "https://movieprototype.firebaseio.com",
-  projectId: "movieprototype",
-  storageBucket: "movieprototype.appspot.com",
-  messagingSenderId: "496661360704",
-  appId: "1:496661360704:web:199c913c73668ef51c6fa5",
+  apiKey: "AIzaSyDVvhlJ-SzTrV9zNcUBO7YBOxpU7jGDCUg",
+  authDomain: "testing123-d7f7d.firebaseapp.com",
+  databaseURL: "https://testing123-d7f7d-default-rtdb.firebaseio.com",
+  projectId: "testing123-d7f7d",
+  storageBucket: "testing123-d7f7d.appspot.com",
+  messagingSenderId: "544452169142",
+  appId: "1:544452169142:web:94670252ff22c4dd72c517"
 };
 
 firebase.initializeApp(config);
